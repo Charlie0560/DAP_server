79d33edee743e2d7e916dfb494e07eecb94d20e9
unknown <chaitanyalokhande87@gmail.com>
2023-02-10 19:11:58 +0530

    Production

---
+++ models/Notices.js
@@ -0,0 +1,46 @@
+var mongoose = require("mongoose");
+
+var Notices = new mongoose.Schema({
+    notice_by:{
+        type:String,
+        required: true
+    }
+    ,
+    heading:{
+        type:String,
+        required: true
+    },
+    desc:{
+        type:String,
+        required: true
+    }
+    ,
+    forw:{
+        type:String,
+        required: true
+    },
+    important:{
+        type: Boolean,
+    },
+    teacher_id:{
+        type:String,
+        required: true
+    },
+    file:{
+        public_id: {
+            type: String,
+          },
+          url: {
+            type: String,
+          },
+    },
+
+    views:{
+        type:Array,
+        default:[]
+    }
+
+},
+{timestamps: true})
+
+module.exports = mongoose.model("Notices",Notices)
